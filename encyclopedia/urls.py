61d97ae364be07492b5a57bf64079fb1d1ad9320
Jonathan <Jonathan106e@gmail.com>
2024-08-26 17:22:00 -0600

    random , edit ya estan terminados

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path("wiki/<str:title>", views.convert, name="entry" ),
     path("search/",views.search, name="search"),
     path("new/",views.new_page,name="new_page"),
+    path("edit/",views.edit, name="editor"),
+    path("save_edit",views.save, name="save"),
+    path("random",views.random_page,name="random_page")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 import markdown2
-
+import random
 from . import util
 
 def index(request):
@@ -53,19 +53,18 @@ def new_page(request):
     if request.method == "GET":
         return render(request,"encyclopedia/new_page.html")
     else:
+        
         title = request.POST['title']
         content = request.POST['content']
 
         title_exist = util.get_entry(title)
-
-        
         
         if title_exist is not None:
             return render(request, "encyclopedia/error.html",{
                 'mensaje':"Esta pagina ya existe."
                 })
         else:
-            header_title = f"# {title}"
+            header_title = f"#{title}"
             pass_mk = header_title + content
              
             util.save_entry(title,pass_mk)
@@ -84,3 +83,57 @@ def new_page(request):
                     'title':title,
                     'html_convert':html_convert,             
                 })
+
+def edit(request):
+    if request.method == "POST":
+        title = request.POST["entry_title"]
+        content = util.get_entry(title)
+
+        return render(request,"encyclopedia/edit.html",{
+            "title":title,
+            "content":content,       
+             })
+
+def save(request):
+    if request.method == "POST":
+        title = request.POST['title']
+        content = request.POST['content']
+
+        util.save_entry(title,content)
+        
+        content = util.get_entry(title)
+        
+        if content == None:
+            
+            return render(request, 'encyclopedia/error.html', {
+                'mensaje': "Fallo tecnico."
+                })
+                
+        else:
+                html_convert = markdown2.markdown(content)
+                
+        return render(request, 'encyclopedia/entry.html',
+                {
+                    'title':title,
+                    'html_convert':html_convert,             
+                })
+
+def random_page(request):
+    entry = util.list_entries()
+    rand_entry = random.choice(entry)
+
+    content = util.get_entry(rand_entry)
+    
+    if content == None:
+        return render(request, 'encyclopedia/error.html', {
+         'mensaje': "La entrada no existe."
+        })
+        
+    else:
+        html_convert = markdown2.markdown(content)
+        
+        return render(request, 'encyclopedia/entry.html',
+        {
+            'title':rand_entry,
+            'html_convert':html_convert,             
+        })
